d9723e94ec03fd022f1fb170f244343f1012300b
AlexcrGomes <alexcustodiorg@gmail.com>
2026-05-12 12:23:38 -0300

    ajuste no teste de criar usuario

--- cypress/e2e/Alex/usuario.add.cy.js
+++ cypress/e2e/Alex/usuario.add.cy.js
@@ -1,16 +1,29 @@
-const { before } = require("mocha");
-
 describe("Teste cadastrar usuario", () => {
 
-  it('Deve cadastrar usuário com sucesso', () => {
-    
+  it("Deve cadastrar usuário com sucesso", () => {
+
     cy.visit("http://localhost:8080")
 
     cy.contains("Usuários").click()
     cy.contains("Cadastrar usuário").click()
 
-    cy.get('input[data-cy="name"]').type("Alex")
-    cy.get('input[data-cy="endereco"]').type("Rua 01, nº 123")
-    cy.get('input[data-cy="email"]').type("alex@example.com")
-    cy.get('input[data-cy="senha"]').type("123456")  })
+    cy.get('input[data-cy="name"]')
+      .type("Alex")
+
+    cy.get('input[data-cy="endereco"]')
+      .type("Rua 01, nº 123")
+
+    cy.get('input[data-cy="email"]')
+      .type("alex@example.com")
+
+    cy.get('input[data-cy="senha"]')
+      .type("123456")
+
+    cy.contains("Enviar").click()
+
+    cy.contains("Novo usuario cadastrado com sucesso!")
+      .should("be.visible")
+
+  })
+
 })
